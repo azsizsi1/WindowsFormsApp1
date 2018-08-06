eaaa5fd7fae67c9cbbf3a0e5493e4617451da4c7
azsizsi1 <42037239+azsizsi1@users.noreply.github.com>
2018-08-06 14:16:01 +0200

    teszt

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Zsizsinstall
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Installoptions = new System.Windows.Forms.CheckedListBox();
+            this.Install_List = new System.Windows.Forms.ListBox();
+            this.isntallstart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Installoptions
+            // 
+            this.Installoptions.CheckOnClick = true;
+            this.Installoptions.FormattingEnabled = true;
+            this.Installoptions.Items.AddRange(new object[] {
+            "Outlook",
+            "Directx",
+            "Appshare"});
+            this.Installoptions.Location = new System.Drawing.Point(12, 12);
+            this.Installoptions.Name = "Installoptions";
+            this.Installoptions.ScrollAlwaysVisible = true;
+            this.Installoptions.Size = new System.Drawing.Size(567, 289);
+            this.Installoptions.TabIndex = 0;
+            //this.Installoptions.SelectedIndexChanged += new System.EventHandler(this.Installoptions_SelectedIndexChanged);
+            // 
+            // Install_List
+            // 
+            this.Install_List.FormattingEnabled = true;
+            this.Install_List.Location = new System.Drawing.Point(585, 12);
+            this.Install_List.Name = "Install_List";
+            this.Install_List.Size = new System.Drawing.Size(195, 264);
+            this.Install_List.TabIndex = 1;
+            // 
+            // isntallstart
+            // 
+            this.isntallstart.Location = new System.Drawing.Point(585, 278);
+            this.isntallstart.Name = "isntallstart";
+            this.isntallstart.Size = new System.Drawing.Size(195, 23);
+            this.isntallstart.TabIndex = 2;
+            this.isntallstart.Text = "Install item(s)";
+            this.isntallstart.UseVisualStyleBackColor = true;
+            this.isntallstart.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Zsizsinstall
+            // 
+            this.ClientSize = new System.Drawing.Size(786, 314);
+            this.Controls.Add(this.isntallstart);
+            this.Controls.Add(this.Install_List);
+            this.Controls.Add(this.Installoptions);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Zsizsinstall";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Zsizsinstall_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Installoptions_SelectedIndexChanged(object sender) //System.Eventargs e)
+        {
+           // throw new NotImplementedException();
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox Installoptions;
+        private System.Windows.Forms.ListBox Install_List;
+        private System.Windows.Forms.Button isntallstart;
     }
 }
-
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,85 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Zsizsinstall : Form
     {
-        public Form1()
+
+
+        private void Zsizsinstall_Load(object sender, EventArgs e)
         {
-            InitializeComponent();
+
         }
+
+        public int CountOfPrograms = 0;
+        string location;
+        string txtline = "";
+
+        public Zsizsinstall()
+        {
+
+            //StreamReader route = File.OpenText("installroutes.txt");
+            //while ((txtline = route.ReadLine()) != null)
+            //{
+            //    string[] array = txtline.Split(';');
+            //    Installoptions.Items.Add(new Tolt(array[0], array[1], array[2]));
+            //    CountOfPrograms++;
+            //}
+            //route.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < CountOfPrograms; i++)
+                {
+                    location = Convert.ToString(txtline[CountOfPrograms * 3]);
+                    Fugg.InstallCall(location);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+    }
+    public class Fugg
+    {
+        public static void InstallCall(string route)
+        {
+            Process install = new Process();
+            install.StartInfo.FileName = @route;
+            install.Start();
+            install.WaitForExit();
+        }
+        public string RouteSearch()
+        {
+
+            return ("");
+        }
+
+    }
+
+    public class Tolt
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Path { get; set; }
+
+        public Tolt(string name, string version, string path)
+        {
+            Name = name;
+            Version = version;
+            Path = path;
+        }
+
     }
 }
